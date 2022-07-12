23ef64872eb6c989d7fb5bc2f66d344be8bc38ca
hepeng <peng6.he@tcl.com>
2022-07-12 10:53:40 +0800

    init project

---
+++ nacos-provider/src/main/java/com/dp/nacos/provider/ProviderBootstrap.java
@@ -0,0 +1,12 @@
+package com.dp.nacos.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderBootstrap.class, args);
+    }
+}
