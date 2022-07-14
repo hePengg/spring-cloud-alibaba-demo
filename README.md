f542aaf7e062cc0f661b4c6ecdcb239ba094cb34
hepeng <peng6.he@tcl.com>
2022-07-14 18:56:52 +0800

    2022-7-14

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # spring-cloud-alibaba-demo
+实践Spring Cloud Alibaba相关技术栈
+
+# TODO
+自定义负载均衡、自定义服务注册&发现，如：
+1. 跨zone负载
+2. 灰度负载
+3. ......
--- nacos-provider/src/main/java/com/dp/nacos/provider/ProviderBootstrap.java
+++ nacos-provider/src/main/java/com/dp/nacos/provider/ProviderBootstrap.java
@@ -2,6 +2,7 @@ package com.dp.nacos.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
 public class ProviderBootstrap {
