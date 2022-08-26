eafa3637b3f5f5223af713a1ee5da4c8876b3273
hepeng <peng6.he@tcl.com>
2022-08-26 12:27:13 +0800

    add webflux

--- webflux/src/main/java/com/dp/webflux/controller/EchoController.java
+++ webflux/src/main/java/com/dp/webflux/controller/EchoController.java
@@ -2,6 +2,7 @@ package com.dp.webflux.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -10,8 +11,13 @@ public class EchoController {
     static String HELLO_WORLD = "Hello World";
 
     @GetMapping("/echo/mono")
-    public Mono<String> echo() {
+    public Mono<String> echoMono() {
         return Mono.just(HELLO_WORLD);
+    }
+
+    @GetMapping("/echo/flux")
+    public Flux<String> echoFlux() {
+        return Flux.just(HELLO_WORLD, HELLO_WORLD, HELLO_WORLD);
     }
 
     @GetMapping("/echo/str")
